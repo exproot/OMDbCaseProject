529a3809973d01f4ba32f7d3361f8c10aa3651f9
Ertan <72192187+exproot@users.noreply.github.com>
2025-06-05 20:21:49 +0300

    Initial project setup (no storyboards) with .gitignore

---
+++ OMDbCaseProject/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  OMDbCaseProject
+//
+//  Created by Ertan Yağmur on 5.06.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .blue
+  }
+
+}
+
